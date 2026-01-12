19fcd5088de0124c32b3e07fac91970bff31ba2f
Koteykee <koteykeethecat@gmail.com>
2026-01-12 13:11:42 +0300

    Header done

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,5 +1,51 @@
-// import styles from "./Header.module.css";
+import { useState } from "react";
+import { Link } from "react-router-dom";
+
+import menuIcon from "../../assets/icons/menu.svg";
+import styles from "./Header.module.css";
 
 export const Header = () => {
-  return <div></div>;
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    if (window.innerWidth < 1000) {
+      setIsMenuOpen((prev) => !prev);
+    }
+  };
+
+  return (
+    <div>
+      <header>
+        <img
+          src={menuIcon}
+          alt="Menu"
+          className={styles["menu"]}
+          onClick={toggleMenu}
+        />
+        <nav
+          className={`${styles["nav-bar"]} ${isMenuOpen ? styles["open"] : ""}`}
+          onClick={toggleMenu}
+        >
+          <Link to="/pomodoro" className={styles["link"]}>
+            Pomodoro
+          </Link>
+          <Link to="/todolist" className={styles["link"]}>
+            Todo List
+          </Link>
+          <Link to="/weather" className={styles["link"]}>
+            Weather
+          </Link>
+          <Link to="/calculator" className={styles["link"]}>
+            Calculator
+          </Link>
+          <Link to="/passwordGenerator" className={styles["link"]}>
+            Password generator
+          </Link>
+        </nav>
+      </header>
+      {isMenuOpen && (
+        <div className={styles["overlay"]} onClick={toggleMenu}></div>
+      )}
+    </div>
+  );
 };
